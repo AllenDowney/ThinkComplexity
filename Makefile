8fe7a487ca64ce7674b6bab56f846079461fa33e
Allen Downey <downey@allendowney.com>
2024-09-24 10:38:25 -0400

    Populating v3

---
+++ Makefile
@@ -0,0 +1,28 @@
+PROJECT_NAME = ThinkComplexity
+PYTHON_VERSION = 3.10
+PYTHON_INTERPRETER = python
+
+create_environment:
+	conda create --name $(PROJECT_NAME) python=$(PYTHON_VERSION) -y
+	@echo ">>> conda env created. Activate with:\nconda activate $(PROJECT_NAME)"
+
+delete_environment:
+	conda env remove --name $(PROJECT_NAME)
+
+requirements:
+	$(PYTHON_INTERPRETER) -m pip install -U pip setuptools wheel
+	$(PYTHON_INTERPRETER) -m pip install -r requirements.txt
+
+clean:
+	find . -type f -name "*.py[co]" -delete
+	find . -type d -name "__pycache__" -delete
+
+lint:
+	flake8 code
+	black --check --config pyproject.toml code
+
+format:
+	black --config pyproject.toml code
+
+tests:
+	cd soln; pytest --nbmake chap*.ipynb
---
+++ soln/Life.py
@@ -0,0 +1,67 @@
+""" Code example from Complexity and Computation, a book about
+exploring complexity science with Python.  Available free from
+
+http://greenteapress.com/complexity
+
+Copyright 2016 Allen Downey
+MIT License: http://opensource.org/licenses/MIT
+"""
+
+import sys
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import animation
+
+"""
+For animation to work in the notebook, you might have to install
+ffmpeg.  On Ubuntu and Linux Mint, the following should work.
+
+    sudo add-apt-repository ppa:mc3man/trusty-media
+    sudo apt-get update
+    sudo apt-get install ffmpeg
+"""
+
+from Cell2D import Cell2D
+from scipy.signal import correlate2d
+
+
+class Life(Cell2D):
+    """Implementation of Conway's Game of Life."""
+
+    kernel = np.array([[1, 1, 1], [1, 10, 1], [1, 1, 1]])
+
+    table = np.zeros(20, dtype=np.uint8)
+    table[[3, 12, 13]] = 1
+
+    def step(self):
+        """Executes one time step."""
+        c = correlate2d(self.array, self.kernel, mode="same")
+        self.array = self.table[c]
+
+
+def main(script, *args):
+    """Constructs a puffer train.
+
+    Uses the entities in this file:
+    http://www.radicaleye.com/lifepage/patterns/puftrain.lif
+    """
+    lwss = ["0001", "00001", "10001", "01111"]
+
+    bhep = ["1", "011", "001", "001", "01"]
+
+    n = 400
+    m = 600
+    life = Life(n, m)
+
+    col = 120
+    life.add_cells(n // 2 + 12, col, *lwss)
+    life.add_cells(n // 2 + 26, col, *lwss)
+    life.add_cells(n // 2 + 19, col, *bhep)
+    anim = life.animate(frames=100)
+    plt.subplots_adjust(left=0.01, right=0.99, bottom=0.01, top=0.99)
+    plt.show()
+
+
+if __name__ == "__main__":
+    main(*sys.argv)
